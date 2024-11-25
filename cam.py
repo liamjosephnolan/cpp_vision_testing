fccfad92bddeede876ef7d13c56df61c24dcfde6
liamjosephnolan <liamjosephnolan@gmail.com>
2024-11-25 15:20:56 +0100

    Distance tracking working with webcam

--- cam.py
+++ cam.py
@@ -2,6 +2,9 @@ import time
 import cv2
 import numpy as np
 
+
+marker_size = 0.1 # meters
+focal_length = 500 # Pixels
 # Initialize the webcam
 cap = cv2.VideoCapture(0)  # 0 is the default webcam, change if you have multiple cameras
 
@@ -11,14 +14,13 @@ if not cap.isOpened():
     exit()
 
 # Load the ArUco dictionary and set parameters
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_7X7_250)
 parameters = cv2.aruco.DetectorParameters()
 detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
 
 print("Press 'q' to quit.")
 
 while True:
-    time.sleep(.25)
 
     # Capture a frame from the webcam
     ret, frame = cap.read()
@@ -37,10 +39,27 @@ while True:
     print(ids)
     # Draw detected markers on the frame
     if ids is not None:
-        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+        for marker_corners in corners:
+            # marker_corners has shape (1, 4, 2)
+            corners_2d = marker_corners[0]  # Extract the (4, 2) array
 
+            # Upper-left corner
+            upper_left = tuple(corners_2d[0])
+
+            # Upper-right corner
+            upper_right = tuple(corners_2d[1])
+        pixel_distance = np.sqrt((upper_right[1] - upper_left[0])**2 + (upper_right[0]- upper_left[1])**2)
+        distance_m = (marker_size * focal_length) / pixel_distance
+        print(upper_left)
+        print(upper_right)
+        print(pixel_distance)
+        print(f"Estimated distance to the marker: {distance_m} meters")
+
+        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
+        cv2.imshow('Detected Markers', frame)
+    else:
     # Show the frame with detected markers
-    cv2.imshow('Detected Markers', gray)
+        cv2.imshow('Detected Markers', gray)
 
     # Exit the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
--- image_test.py
+++ image_test.py
@@ -7,14 +7,7 @@ image = cv2.imread('high_rez.jpg')
 # Convert the image to grayscale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-gray = cv2.equalizeHist(gray)
-
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-
-alpha = .5 # Contrast control
-gray = cv2.convertScaleAbs(gray, alpha=alpha)
-
-aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_250)
 parameters = cv2.aruco.DetectorParameters()
 
 # Create the ArUco detector
